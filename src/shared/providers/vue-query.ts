16c7fc5955453b8270e5e356b70950d72b72f391
Administrator <administrator@MacBookPro.home>
2026-05-04 19:56:31 +0200

    increase the stale time

--- src/shared/providers/vue-query.ts
+++ src/shared/providers/vue-query.ts
@@ -1,10 +1,12 @@
 import { QueryClient } from '@tanstack/vue-query'
 
+const FIVE_MINUTES_MS = 1000 * 60 * 5
+
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       retry: 1,
-      staleTime: 1000 * 60,
+      staleTime: FIVE_MINUTES_MS,
       refetchOnWindowFocus: false,
     },
   },
